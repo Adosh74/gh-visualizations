0db5a70eadfbdac8f5418b2efe17010a96bdb16d
Mohamed Shebl <98131964+Adosh74@users.noreply.github.com>
2026-03-22 03:19:37 +0200

    Update server initialization to include database connection; log errors during startup

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,22 @@ import os from 'node:os';
 
 import { app } from './app';
 import { serverEnv } from './config';
+import { initDb } from './datastore/index-dao';
 import { LOGGER } from './logging';
 
 const port = serverEnv.port;
 const hostname = os.hostname();
 
-app.listen(port, () => {
-  LOGGER.info(`Hostname: ${hostname}`);
+(async () => {
+  try {
+    await initDb();
+    app.listen(port, () => {
+      LOGGER.info(`Hostname: ${hostname}`);
 
-  LOGGER.info(`server running on port ${port} in ${serverEnv.nodeEnv} mode`);
-});
+      LOGGER.info(`server running on port ${port} in ${serverEnv.nodeEnv} mode`);
+    });
+  }
+  catch (error) {
+    LOGGER.error(error);
+  }
+})();
