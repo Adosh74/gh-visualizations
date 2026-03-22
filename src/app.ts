5c76b1bb7adf1bb2ba449bb7044be7b710528705
Mohamed Shebl <98131964+Adosh74@users.noreply.github.com>
2026-03-23 00:39:08 +0200

    Add API routing to integrate index routes into the application

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import express from 'express';
 
 import { NotFound } from './errors/not-found-error';
 import { errorHandler } from './middlewares/error-handler';
+import { APIs } from './routes/index-route';
 
 const app = express();
 
@@ -12,6 +13,8 @@ app.use(express.json());
 app.get('/healthz', (_req: Request, res: Response) => {
   res.sendStatus(200);
 });
+
+app.use('/api/v1', APIs);
 
 app.use((_req: Request, _res: Response, next) => {
   const error = new NotFound();
